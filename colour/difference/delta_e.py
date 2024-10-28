mesvam/colour

commit 07b4e288ba30b144c2b55f83e47d455a7e51d100

make metric difference consistent with original paper

colour/difference/delta_e.py
@@ -615,7 +615,7 @@ def delta_E_HyAB(Lab_1: ArrayLike, Lab_2: ArrayLike) -> NDArrayFloat:
     >>> delta_E_HyAB(Lab_1, Lab_2)  # doctest: +ELLIPSIS
     102.4309069...
     """
-    dLab = to_domain_100(Lab_2) - to_domain_100(Lab_1)
+    dLab = to_domain_100(Lab_1 - Lab_2)
     dL, da, db = tsplit(dLab)
     HyAB = np.abs(dL) + np.sqrt(da**2 + db**2)
     return HyAB

commit b619509ef740b88faa97027c308298677f85f929

fix incorrect handing of input arrays introduced by 07b4e288b

colour/difference/delta_e.py
@@ -615,7 +615,7 @@ def delta_E_HyAB(Lab_1: ArrayLike, Lab_2: ArrayLike) -> NDArrayFloat:
     >>> delta_E_HyAB(Lab_1, Lab_2)  # doctest: +ELLIPSIS
     102.4309069...
     """
-    dLab = to_domain_100(Lab_1 - Lab_2)
+    dLab = to_domain_100(Lab_1) - to_domain_100(Lab_2)
     dL, da, db = tsplit(dLab)
     HyAB = np.abs(dL) + np.sqrt(da**2 + db**2)
     return as_float(HyAB)